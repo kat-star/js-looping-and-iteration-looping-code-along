a81268a326b91f7950bf6f58451cb9a8919a2680
kat-star <katherinewkwong@gmail.com>
2019-10-27 16:35:42 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(arr, event) {
+  const new_arr = []
+  for (let i = 0; i < arr.length; i++) {
+    new_arr.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`);
+  }
+  return new_arr;
+}
+
+function countDown(num) {
+  let i = 0
+  while (i <= num) {
+    console.log(i);
+    i ++;
+  }
+}
+
